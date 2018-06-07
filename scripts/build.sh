926ddc842a0c296ba8faf773965868c2dce19d22
Douglas Lockamy <dlockamy@macbase12.local>
2018-06-07 19:51:32 -0400

    Restruct project to follow golang workspace norms.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-cd listen
+cd cmd/listen
 env GOOS=linux  go build -o listen main.go
 docker build -t local/listen .
 rm listen
 cd ..
 
-cd upload
+cd cmd/upload
 env GOOS=linux  go build -o upload main.go 
 docker build -t local/upload .
 rm upload
 cd ..
 
-cd processor
+cd cmd/processor
 env GOOS=linux  go build -o processor main.go 
 docker build -t local/processor .
 rm processor
 cd ..
 
-cd webclient
-env GOOS=linux  go build -o webclient main.go 
-cp -fr ../www ./
-docker build -t local/webclient .
-rm webclient
+cd cmd/web
+env GOOS=linux  go build -o web main.go 
+cp -fr www cmd/web/www
+docker build -t local/web .
+rm web
 rm -fr ./www 
 cd ..
